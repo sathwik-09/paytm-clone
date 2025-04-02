d919831560aae1a588be68728916069d6daaba56
sathwik <enapothulasathwik09@gmail.com>
2025-04-02 11:50:22 +0530

    Set up Express && MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const dotenv = require("dotenv");
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+const appRouter = require('./routes/index');
+
+
+app.use("/api/v1", appRouter);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+app.listen(process.env.PORT, () => {
+  connectDB();
+  console.log("Server is running on port: " + process.env.PORT);
+});
