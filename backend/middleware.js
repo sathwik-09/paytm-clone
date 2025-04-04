9ac148c30ddb683d2e124d008584ae51cb93ea3e
sathwik <enapothulasathwik09@gmail.com>
2025-04-04 22:08:14 +0530

    Fix issue in middleware.js

--- backend/middleware.js
+++ backend/middleware.js
@@ -4,12 +4,12 @@ const authMiddleware = (req, res, next) => {
   const authHeader  = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(403).json({});
-  }
+  } 
   const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if(decoded.userId) {
-      req.userId = decoded.userId;
+    if(decoded.id) {
+      req.userId = decoded.id;
       next();
     } else {
       res.status(401).json({
